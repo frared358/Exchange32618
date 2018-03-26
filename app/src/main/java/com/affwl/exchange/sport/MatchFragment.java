d4ba058beff972b2e8b90fafd308dd654d8df3d0
frared358 <frared358@gmail.com>
2018-03-26 15:44:30 +0530

    Fx Files

--- app/src/main/java/com/affwl/exchange/sport/MatchFragment.java
+++ app/src/main/java/com/affwl/exchange/sport/MatchFragment.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.affwl.exchange.sport;
 
 
@@ -23,29 +23,3 @@ public class MatchFragment extends Fragment {
     }
 
 }
-=======
-package com.affwl.exchange.sport;
-
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.affwl.exchange.R;
-
-
-public class MatchFragment extends Fragment {
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_match, container, false);
-    }
-
-}
->>>>>>> app name to TEJAS
