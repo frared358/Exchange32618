973846f84fde4fe9ad91fabf75270f03bcf33d4d
telangdeep <telangdip@gmail.com>
2018-03-27 20:42:38 +0530

    fx update

--- app/src/main/java/com/affwl/exchange/fx/Problem_Description.java
+++ app/src/main/java/com/affwl/exchange/fx/Problem_Description.java
@@ -2,6 +2,7 @@ package com.affwl.exchange.fx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.affwl.exchange.R;
 
@@ -11,5 +12,13 @@ public class Problem_Description extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem__description);
+        android.support.v7.widget.Toolbar toolbar=findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.journal1_menu, menu);
+        return true;
     }
 }
