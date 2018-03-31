bd14dc96313203c0216273e41d6ff5d7ce47847c
ravikant55 <gravikant55@gmail.com>
2018-03-31 21:51:16 +0530

    fx update 31/3

--- app/src/main/java/com/affwl/exchange/fx/CustomSpinner.java
+++ app/src/main/java/com/affwl/exchange/fx/CustomSpinner.java
@@ -1,5 +1,6 @@
 package com.affwl.exchange.fx;
 
+import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -10,6 +11,8 @@ import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,10 +33,15 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class CustomSpinner extends AppCompatActivity implements  OnChartValueSelectedListener {
+public class CustomSpinner extends AppCompatActivity implements  OnChartValueSelectedListener,View.OnClickListener {
 
     private LineChart mChart;
     protected Typeface mTfLight;
+
+    int mInteger=0;
+
+    EditText displayInteger;
+    ImageView imgVIncrementRed;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,6 +72,15 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
 
         Spinner SpinnerExample = (Spinner) findViewById (R.id.spinner);
         //output = (TextView) findViewById(R.id.output);
+
+        displayInteger =  findViewById (R.id.redValue);
+        imgVIncrementRed = findViewById (R.id.imgVIncrementRed);
+        imgVIncrementRed.setOnClickListener (new View.OnClickListener (){
+            @Override
+            public void onClick(View v) {
+                incrementRed();
+            }
+        });
 
         setListData ();
 
@@ -116,7 +133,7 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
         mChart.setPinchZoom(true);
 
         // set an alternative background color
-        mChart.setBackgroundColor(Color.LTGRAY);
+        //mChart.setBackgroundColor(Color.LTGRAY);
 
         // add data
         setData(20, 30);
@@ -130,7 +147,7 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
         l.setForm(LegendForm.LINE);
         l.setTypeface(mTfLight);
         l.setTextSize(11f);
-        l.setTextColor(Color.WHITE);
+        l.setTextColor(Color.GRAY);
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
         l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
@@ -186,13 +203,13 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
 //            }
             }
 
-            ArrayList<Entry> yVals3 = new ArrayList<Entry>();
-
-            for (int i = 0; i < count; i++) {
-                float mult = range;
-                float val = (float) (Math.random() * mult) + 500;
-                yVals3.add(new Entry(i, val));
-            }
+//            ArrayList<Entry> yVals3 = new ArrayList<Entry>();
+//
+//            for (int i = 0; i < count; i++) {
+//                float mult = range;
+//                float val = (float) (Math.random() * mult) + 500;
+//                yVals3.add(new Entry(i, val));
+//            }
 
             LineDataSet set1, set2, set3;
 
@@ -200,56 +217,81 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
                     mChart.getData().getDataSetCount() > 0) {
                 set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
                 set2 = (LineDataSet) mChart.getData().getDataSetByIndex(1);
-                set3 = (LineDataSet) mChart.getData().getDataSetByIndex(2);
+               //set3 = (LineDataSet) mChart.getData().getDataSetByIndex(2);
                 set1.setValues(yVals1);
                 set2.setValues(yVals2);
-                set3.setValues(yVals3);
+               // set3.setValues(yVals3);
                 mChart.getData().notifyDataChanged();
                 mChart.notifyDataSetChanged();
+
+                /**   remove circle on lineChart   */
+                set1.setDrawCircles (false);
+                set2.setDrawCircles (false);
+               // set3.setDrawCircles (false);
+
+                /**   remove curve on lineChart   */
+                set1.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+                set2.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+                //set3.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+
+                /**   remove VALUES on lineChart   */
+                set1.setDrawValues(false);
+                set2.setDrawValues(false);
+                //set3.setDrawValues(false);
+
             } else {
                 // create a dataset and give it a type
-                set1 = new LineDataSet(yVals1, "DataSet 1");
+                set1 = new LineDataSet(yVals1, "TP");
 
                 set1.setAxisDependency(AxisDependency.LEFT);
                 set1.setColor(ColorTemplate.getHoloBlue());
                 set1.setCircleColor(Color.WHITE);
                 set1.setLineWidth(2f);
-                set1.setCircleRadius(3f);
-                set1.setFillAlpha(65);
+//                set1.setCircleRadius(3f);
+//                set1.setFillAlpha(65);
                 set1.setFillColor(ColorTemplate.getHoloBlue());
                 set1.setHighLightColor(Color.rgb(244, 117, 117));
                 set1.setDrawCircleHole(false);
+                set1.setDrawCircles (false);
+                set1.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+                set1.setDrawValues(false);
                 //set1.setFillFormatter(new MyFillFormatter(0f));
                 //set1.setDrawHorizontalHighlightIndicator(false);
                 //set1.setVisible(false);
                 //set1.setCircleHoleColor(Color.WHITE);
 
                 // create a dataset and give it a type
-                set2 = new LineDataSet(yVals2, "DataSet 2");
+                set2 = new LineDataSet(yVals2, "SL");
                 set2.setAxisDependency(AxisDependency.RIGHT);
                 set2.setColor(Color.RED);
                 set2.setCircleColor(Color.WHITE);
                 set2.setLineWidth(2f);
-                set2.setCircleRadius(3f);
-                set2.setFillAlpha(65);
+                //set2.setCircleRadius(3f);
+                //set2.setFillAlpha(65);
                 set2.setFillColor(Color.RED);
                 set2.setDrawCircleHole(false);
+                set2.setDrawCircles (false);
+                set2.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+                set2.setDrawValues(false);
                 set2.setHighLightColor(Color.rgb(244, 117, 117));
                 //set2.setFillFormatter(new MyFillFormatter(900f));
 
-                set3 = new LineDataSet(yVals3, "DataSet 3");
-                set3.setAxisDependency(AxisDependency.RIGHT);
-                set3.setColor(Color.YELLOW);
-                set3.setCircleColor(Color.WHITE);
-                set3.setLineWidth(2f);
-                set3.setCircleRadius(3f);
-                set3.setFillAlpha(65);
-                set3.setFillColor(ColorTemplate.colorWithAlpha(Color.YELLOW, 200));
-                set3.setDrawCircleHole(false);
-                set3.setHighLightColor(Color.rgb(244, 117, 117));
+//                set3 = new LineDataSet(yVals3, "DataSet 3");
+//                set3.setAxisDependency(AxisDependency.RIGHT);
+//                set3.setColor(Color.YELLOW);
+//                set3.setCircleColor(Color.WHITE);
+//                set3.setLineWidth(2f);
+//                set3.setCircleRadius(3f);
+//                set3.setFillAlpha(65);
+//                set3.setFillColor(ColorTemplate.colorWithAlpha(Color.YELLOW, 200));
+//                set3.setDrawCircleHole(false);
+//                set3.setDrawCircles (false);
+//                set3.setMode (LineDataSet.Mode.CUBIC_BEZIER);
+//                set3.setDrawValues(false);
+//                set3.setHighLightColor(Color.rgb(244, 117, 117));
 
                 // create a data object with the datasets
-                LineData data = new LineData(set1, set2, set3);
+                LineData data = new LineData(set1, set2);
                 data.setValueTextColor(Color.WHITE);
                 data.setValueTextSize(9f);
 
@@ -298,6 +340,36 @@ public class CustomSpinner extends AppCompatActivity implements  OnChartValueSel
 
 
 
+   /** Increment/Decrement  for Red value */
+   public  void incrementRed()
+   {
+       mInteger = mInteger + 1;
+       display(mInteger);
+   }
+
+
+    public  void decrementRed(View view)
+    {
+        mInteger = mInteger - 1;
+        display(mInteger);
+    }
+
+    private void  display(int number)
+   {
+       Log.i ("TAGf",""+number);
+       try {
+           displayInteger.setText(String.valueOf (number));
+       } catch (Exception e) {
+           e.printStackTrace ();
+           Log.i("TAGf",""+e);
+       }
+   }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
 
 
