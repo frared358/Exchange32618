984cbe0d1193719446f7fc3f6d0894f1053f40d7
telangdeep <telangdip@gmail.com>
2018-04-04 21:14:00 +0530

    update final

--- app/src/main/java/com/affwl/exchange/fx/Add_symbol_Forex.java
+++ app/src/main/java/com/affwl/exchange/fx/Add_symbol_Forex.java
@@ -1,15 +1,25 @@
 package com.affwl.exchange.fx;
 
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.affwl.exchange.R;
 
 public class Add_symbol_Forex extends AppCompatActivity {
    LinearLayout ll_add_symbol;
+    ImageView imageView8;
+    AutoCompleteTextView autotv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +27,26 @@ public class Add_symbol_Forex extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
 
-
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.web_search, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.search) {
+            autotv1=(AutoCompleteTextView)findViewById(R.id.autotv1);
+            autotv1.setVisibility(View.VISIBLE);
+            imageView8=(ImageView)findViewById(R.id.imageView8);
+            imageView8.setVisibility(View.VISIBLE);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
