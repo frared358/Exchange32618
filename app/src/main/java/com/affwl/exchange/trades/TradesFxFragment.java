469fc32acb9a4f42b712c23fcb7f20efaf97a146
saritaNadar <nsr400104@gmail.com>
2018-04-20 16:41:32 +0530

    worked on sixtySec Trade

--- app/src/main/java/com/affwl/exchange/trades/TradesFxFragment.java
+++ app/src/main/java/com/affwl/exchange/trades/TradesFxFragment.java
@@ -1,27 +1,57 @@
 package com.affwl.exchange.trades;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.affwl.exchange.R;
 
-public class TradesFxFragment extends Fragment {
+public class TradesFxFragment extends Fragment implements View.OnClickListener {
 
 
     public TradesFxFragment() {
         // Required empty public constructor
     }
 
+    LinearLayout ll_position_details,ll_position_list;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trades_fx, container, false);
+
+
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ll_position_list=view.findViewById(R.id.ll_position_list);
+        ll_position_details=view.findViewById(R.id.ll_position_details);
+
+        ll_position_list.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.ll_position_list:
+                if(ll_position_details.getVisibility()==View.VISIBLE)
+                {
+                    ll_position_details.setVisibility(View.GONE);
+                }
+                else {
+                    ll_position_details.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
 }
 
--- app/src/main/java/com/affwl/exchange/trades/TradesSixtySecFragment.java
+++ app/src/main/java/com/affwl/exchange/trades/TradesSixtySecFragment.java
@@ -1,15 +1,21 @@
 package com.affwl.exchange.trades;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.affwl.exchange.R;
 
-public class TradesSixtySecFragment extends Fragment {
+public class TradesSixtySecFragment extends Fragment implements View.OnClickListener {
 
+
+    ImageView img_announce;
+    LinearLayout ll_announce_details;
 
     public TradesSixtySecFragment() {
         // Required empty public constructor
@@ -23,5 +29,28 @@ public class TradesSixtySecFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_trades_sixtysec, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ll_announce_details=view.findViewById(R.id.ll_announce_details);
+        img_announce=view.findViewById(R.id.img_announce);
+        img_announce.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_announce:
+                if(ll_announce_details.getVisibility()==View.VISIBLE)
+                {
+                    ll_announce_details.setVisibility(View.GONE);
+                }
+                else {
+                    ll_announce_details.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
 }
 
