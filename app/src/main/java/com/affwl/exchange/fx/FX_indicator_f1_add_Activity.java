e7a2388f91e23fbedbff34e813d33cf8020d577a
ravikant55 <gravikant55@gmail.com>
2018-05-21 14:32:20 +0530

    fx update 21-5-18

--- app/src/main/java/com/affwl/exchange/fx/FX_indicator_f1_add_Activity.java
+++ app/src/main/java/com/affwl/exchange/fx/FX_indicator_f1_add_Activity.java
@@ -19,8 +19,6 @@ public class FX_indicator_f1_add_Activity extends AppCompatActivity implements O
 
 Button btncolorpicker;
 
-
-
 RelativeLayout mylayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,45 +39,7 @@ RelativeLayout mylayout;
         new ColorPickerOval(this, this, "", Color.WHITE).show();
     }
 
-//        btncolorpicker=(Button)findViewById(R.id.btncolorpicker);
-//
-//
-//        mylayout=(RelativeLayout)findViewById(R.id.mylayout);
-//
-//
 
-
-//    public void openColorPicker()
-//    {
-//        final ColorPicker ColorPicker =new ColorPicker(this);
-//        ArrayList<String> colors= new ArrayList<>();
-//        colors.add("#258174");
-//        colors.add("#3C8D2F");
-//        colors.add("#20724F");
-//        colors.add("#6a3ab2");
-//        colors.add("#323299");
-//        colors.add("#000000");
-//        colors.add("#808000");
-//        colors.add("#966d37");
-//        colors.add("#b77231");
-//        colors.add("#258174");
-//
-//        ColorPicker.setColors(colors)
-//                .setColumns(5)
-//                .setRoundColorButton(true)
-//        .setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
-//            @Override
-//            public void onChooseColor(int position, int color) {
-//                mylayout.setBackgroundColor(color);
-//            }
-
-//            @Override
-//            public void onCancel() {
-//
-//            }
-//        })
-//                .show();
-//    }
 
     @Override
     public void colorChanged(String key, int color) {
